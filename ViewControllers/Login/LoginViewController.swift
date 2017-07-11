f179176d81570bbcd2d29f01c98cc28055de4a38
trnhgquan <trhquan.kmhd@gmail.com>
2017-07-11 09:15:16 +0700

    initial commit

--- ViewControllers/Login/LoginViewController.swift
+++ ViewControllers/Login/LoginViewController.swift
@@ -9,11 +9,16 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
 
     @IBOutlet weak var loginView: UIView!
     @IBOutlet weak var logoLabel: UILabel!
     @IBOutlet weak var loginLabel: UILabel!
     
+    @IBOutlet weak var btnLogin: UIButton!
+    
+    @IBOutlet weak var passwordTf: UITextField!
+    @IBOutlet weak var emailTf: UITextField!
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -24,8 +29,44 @@ class LoginViewController: UIViewController {
         loginLabel.backgroundColor = AMSColor.redColor
         loginLabel.textColor = .white
         loginLabel.sizeToFit()
+        loginView.backgroundColor = .white
+        btnLogin.backgroundColor = AMSColor.redColor
         // Do any additional setup after loading the view.
     }
     
-   
+    @IBAction func loginBtn(_ sender: Any) {
+        let email = emailTf.text
+        let password = passwordTf.text
+        if  isEmailExist(email: email!) {
+            if checkPassword(password: password!) {
+                let alertController = UIAlertController(title: "Login", message: "Successfully", preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "OK", style: .cancel , handler: nil))
+                present(alertController, animated: true, completion: nil)
+            }
+            else{
+                let alertController = UIAlertController(title: "Login", message: "Password incorrect", preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "OK", style: .cancel , handler: nil))
+                present(alertController, animated: true, completion: nil)
+            }
+           
+        }
+        else {
+            let alertController = UIAlertController(title: "Login", message: "Email incorrect", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .cancel , handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    func isEmailExist(email: String) -> Bool{
+        if email == "String" {
+            return true
+        }
+        return false
+    }
+    func checkPassword(password: String)->Bool{
+        if password.characters.count == 6{
+            return true
+        }
+        return false
+    }
 }
